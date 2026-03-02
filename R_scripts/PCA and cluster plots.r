ceecb712ca6f543db6c9c098722c641f401ee5f1
williamahlstrom8-alt <william.ahlstrom8@gmail.com>
2026-03-02 09:29:46 +0100

    Jag tog bort onödig kod

--- R_scripts/PCA and cluster plots.r
+++ R_scripts/PCA and cluster plots.r
@@ -6,32 +6,6 @@ library('poppr')
 # load the genlight data
 load("eelgrass_data/genlight_zostera_data.rda")
 
-#Deal with replicates####
-#obtain individual names of samples (some are replicates)
-indNames(gl_zostera_25)
-
-##Identify and remove replicates####
-# Identify individuals that are NOT replicates
-keep <- !grepl("-rep$", indNames(gl_zostera_25))
-
-# Subset the genlight object
-gl_zostera_no_rep <- gl_zostera_25[keep, ]
-
-#number of replicates
-sum(grepl("-rep$", indNames(gl_zostera_25)))  
-
-#number of sampled individuals (no replicates)
-nInd(gl_zostera_no_rep)
-
-#number of samples per region
-table(pop(gl_zostera_no_rep))
-
-#convert genlight to snpclone####
-#we will use snpclone in the rest of the project
-snp_zostera <- as.snpclone(gl_zostera_no_rep)
-
-#Save snp_zostera in the folder for manipulated data 
-saveRDS(snp_zostera, file = "eelgrass_data/Manipulated_data/snp_zostera.rds")
 
 #Load packages and data####
 #packages
