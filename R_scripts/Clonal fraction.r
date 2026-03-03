6f51210b3deb5090631ea88e2694f3b4d979b807
Rasmus Green <rasmus.green@hotmail.com>
2026-03-03 09:26:52 +0100

    Code for clonal fraction

---
+++ R_scripts/Clonal fraction.r
@@ -0,0 +1,62 @@
+#Load packages and data####
+#packages
+library("hierfstat")
+library("pegas")
+library("adegenet")
+library("poppr")
+library("tidyverse")
+library("assignPOP")
+library("radiator")
+
+library(poppr)
+
+sessionInfo()
+#data
+snp_zostera <- readRDS("eelgrass_data/Manipulated_data/snp_zostera.rds")
+class(snp_zostera)
+
+#plot nmll over genetic distance cutoff, and three threshold methods
+zost_filtered <- filter_stats(snp_zostera, distance = bitwise.dist, plot = TRUE)
+
+###different ways of calculating thresholds####
+#Farthest threshold, should be used (Largest value)
+print(farthest_thresh <- cutoff_predictor(zost_filtered$farthest$THRESHOLDS))
+
+#Average threshold, should NOT be used (Smallest value)
+print(average_thresh  <- cutoff_predictor(zost_filtered$average$THRESHOLDS))
+
+#Nearest threshold, same as largest for some reason (idk why)
+print(nearest_thresh  <- cutoff_predictor(zost_filtered$nearest$THRESHOLDS))
+
+#MLG filter UPGMA####
+#apply the filter
+mlg.filter(snp_zostera, distance = bitwise.dist, algorithm = "a") <- average_thresh
+
+snp_zostera
+#returns number of MLGs 
+mlg(snp_zostera)
+
+# Skapa MLG tabell #
+
+mlg_table <- poppr::mlg.table(snp_zostera, plot = FALSE)
+dim(mlg_table)
+
+# Definierar funktion #
+
+myCF <- function(x){
+  x <- drop(as.matrix(x))
+  if (length(dim(x)) > 1){
+    res <- rowSums(x > 0) / rowSums(x)
+  } else {
+    res <- sum(x > 0) / sum(x)
+  }
+  return(res)
+}
+
+# Räkna ut klonal fraktion #
+
+myCF(mlg_table)
+
+# Klassisk klonal fraktion #
+
+1 - myCF(mlg_table)
