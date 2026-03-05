862a6d28391a2b600c41d4677f2a612aaca9dda5
williamahlstrom8-alt <william.ahlstrom8@gmail.com>
2026-03-05 11:04:29 +0100

    Jag skapade ett skript för t-tester och la till salinitetskartan

---
+++ R_scripts/t_tester.R
@@ -0,0 +1,46 @@
+#Packages and data####
+library(readr)
+goodCF <- read_csv("eelgrass_data/Manipulated_data/goodCF.csv")
+
+#t-tests####
+##split = ALA####
+# exempel på t-test formel
+#t.test(formula, data, subset, na.action, …)
+# Create group belonging
+goodCF$group <- ifelse(goodCF$Population %in% c("KOD","STE","GOT","GRO","HOG","ALA"),
+                       "west", "east")
+
+#run t-test
+t.test(ClonalFraction ~ group, data = goodCF, alternative = "two.sided")
+
+#create boxplot 
+boxplot(ClonalFraction ~ group, data = goodCF)
+
+#Reload data to remove group
+goodCF <- read_csv("eelgrass_data/Manipulated_data/goodCF.csv")
+
+##split = HOG####
+goodCF$group <- ifelse(goodCF$Population %in% c("KOD","STE","GOT","GRO","HOG"),
+                       "west", "east")
+
+#run t-test
+t.test(ClonalFraction ~ group, data = goodCF, alternative = "two.sided")
+
+#create boxplot 
+boxplot(ClonalFraction ~ group, data = goodCF)
+
+#Reload data to remove group
+goodCF <- read_csv("eelgrass_data/Manipulated_data/goodCF.csv")
+
+##split = GOT####
+goodCF$group <- ifelse(goodCF$Population %in% c("KOD","STE","GOT"),
+                       "west", "east")
+
+#run t-test
+t.test(ClonalFraction ~ group, data = goodCF, alternative = "two.sided")
+
+#create boxplot 
+boxplot(ClonalFraction ~ group, data = goodCF)
+
+#Reload data to remove group
+goodCF <- read_csv("eelgrass_data/Manipulated_data/goodCF.csv")
